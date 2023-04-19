112c7d8f71b4cb950f9781ec8a5a321e58e11bcd
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-04-19 16:01:50 +0300

    onAuthStateChanged practiced

--- src/components/providers/AuthProviders.jsx
+++ src/components/providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../../Firebase/firebase.config';
 
 
@@ -16,13 +16,35 @@ const AuthProviders = ({ children }) => {
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
     }
+
     const signIn = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
     }
+
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+
+    // observe auth state change
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('auth state change', currentUser);
+            setUser(currentUser);
+        });
+
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+
     const authInfo = {
         user,
         createUser,
-        signIn
+        signIn,
+        signOut
     }
 
     return (
