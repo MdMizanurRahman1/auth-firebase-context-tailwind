9b03d6bc39d19547ea55ca4e438294a333f0d34e
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-04-19 14:22:26 +0300

    login, register, context api and auth provider practiced

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDq2i04ZUgBcl4jictuxNcdftdylmGp2sA",
+    authDomain: "auth-firebase-context-ta-c1b41.firebaseapp.com",
+    projectId: "auth-firebase-context-ta-c1b41",
+    storageBucket: "auth-firebase-context-ta-c1b41.appspot.com",
+    messagingSenderId: "986243396993",
+    appId: "1:986243396993:web:66153e1570709438356480"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+
+
+    const handleLogIn = event => {
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+    }
+
+
+
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center">
+                    <h1 className="text-5xl font-bold">Please login!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogIn} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name='email' placeholder="Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="Password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                    <label className="label mx-auto mb-2">
+                        <Link to="/register" className="label-text-alt link link-hover">New to auth master? Please Register!</Link>
+                    </label>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
